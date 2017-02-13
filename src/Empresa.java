9310c2f705e601ca6216b46c7a6a6b66fbeaa955
Alumno <Alumno@Equipo-415>
2017-02-13 13:29:23 +0100

    Commit 3 implementar interfaz

---
+++ src/Empresa.java
@@ -0,0 +1,94 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+/**
+ *
+ * @author Alumno
+ */
+public class Empresa {
+    private String identificador;
+    private String nombre;
+    private String cambio;
+    private String porcentaje;
+    private boolean verde;
+    
+    public Empresa(String i,String n,String c,String p,boolean v){
+        this.identificador=i;
+        this.nombre=n;
+        this.cambio=c;
+        this.porcentaje=p;
+        this.verde=v;
+    }
+
+    /**
+     * @return the identificador
+     */
+    public String getIdentificador() {
+        return identificador;
+    }
+
+    /**
+     * @param identificador the identificador to set
+     */
+    public void setIdentificador(String identificador) {
+        this.identificador = identificador;
+    }
+
+    /**
+     * @return the nombre
+     */
+    public String getNombre() {
+        return nombre;
+    }
+
+    /**
+     * @param nombre the nombre to set
+     */
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    /**
+     * @return the cambio
+     */
+    public String getCambio() {
+        return cambio;
+    }
+
+    /**
+     * @param cambio the cambio to set
+     */
+    public void setCambio(String cambio) {
+        this.cambio = cambio;
+    }
+
+    /**
+     * @return the porcentaje
+     */
+    public String getPorcentaje() {
+        return porcentaje;
+    }
+
+    /**
+     * @param porcentaje the porcentaje to set
+     */
+    public void setPorcentaje(String porcentaje) {
+        this.porcentaje = porcentaje;
+    }
+
+    /**
+     * @return the verde
+     */
+    public boolean isVerde() {
+        return verde;
+    }
+
+    /**
+     * @param verde the verde to set
+     */
+    public void setVerde(boolean verde) {
+        this.verde = verde;
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -4,22 +4,9 @@ import javax.swing.JOptionPane;
 public class Main {
 	
 	public static void main(String[] args){
-		int n = askInt("Numero de poste: "); //103 poste de los enlaces
-		
-		Poste p = new Poste(n);
-		List<Poste.Bus> buses = p.getBuses();
+		Conexion p = new Conexion();
 		Thread t1 = new Thread(p);
 		t1.start();
-	}
-	
-	
-	private static int askInt(String str){
-		try{
-			return Integer.parseInt(JOptionPane.showInputDialog(str));
-		}
-		catch (Exception e){
-			return askInt(str);
-		}
 	}
 
 }
